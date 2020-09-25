364e48bd9e0ceaed67084af2a9ac4c31dcc7cbf6
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2020-09-25 04:08:10 -0400

    initial changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `thost.dev`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Tim Hostetler`,
+      summary: `who lives and works in Boston building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
